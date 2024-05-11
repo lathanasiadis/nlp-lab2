b3e91ddc54ebd9c136ddf39c745aca7e88ff5ac3
Angelos <angelos353@gmail.com>
2024-05-11 11:19:47 +0300

    some pretrained models

--- transfer_pretrained.py
+++ transfer_pretrained.py
@@ -3,12 +3,34 @@ from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 from utils.load_datasets import load_MR, load_Semeval2017A
 from training import get_metrics_report
+import argparse
 
-# DATASET = 'MR'
-# PRETRAINED_MODEL = 'siebert/sentiment-roberta-large-english'
 
-DATASET = 'Semeval2017A'
-PRETRAINED_MODEL = 'cardiffnlp/twitter-roberta-base-sentiment'
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--model",
+    choices=["twitter_roberta", "roberta_large", "bertweet", "financial"],
+    help="""Pretrained model""",
+    default="twitter_roberta"
+)
+parser.add_argument("-d", "--dataset",
+    choices=["mr", "semeval"],
+    help="""Dataset used to train and evaluate the model.""",
+    default="semeval"
+)
+
+args = parser.parse_args()
+
+
+args_to_models = {"twitter_roberta": 'cardiffnlp/twitter-roberta-base-sentiment',
+                  "roberta_large": 'siebert/sentiment-roberta-large-english',
+                  "bertweet": "finiteautomata/bertweet-base-sentiment-analysis",
+                  "financial": "ahmedrachid/FinancialBERT-Sentiment-Analysis"}
+                  
+
+PRETRAINED_MODEL = args_to_models[args.model]
+
+DATASET = "MR" if args.dataset == "mr" else "Semeval2017A"
 
 
 LABELS_MAPPING = {
@@ -20,7 +42,22 @@ LABELS_MAPPING = {
         'LABEL_0': 'negative',
         'LABEL_1': 'neutral',
         'LABEL_2': 'positive',
-    }
+    },
+    'finiteautomata/bertweet-base-sentiment-analysis': {
+        'POS': 'positive',
+        'NEG': 'negative',
+        'NEU': 'neutral',
+    },
+    "soleimanian/financial-roberta-large-sentiment": {
+        "positive": "positive",
+        "negative": "negative",
+        "neutral": "neutral",
+    },
+    "ahmedrachid/FinancialBERT-Sentiment-Analysis": {
+        "positive": "positive",
+        "negative": "negative",
+        "neutral": "neutral",
+    },
 }
 
 if __name__ == '__main__':
@@ -44,8 +81,8 @@ if __name__ == '__main__':
 
     y_pred = []
     for x in tqdm(X_test):
-        # TODO: Main-lab-Q6 - get the label using the defined pipeline 
-        label = ...        
+        # TODO: Main-lab-Q6 - get the label using the defined pipeline
+        label = sentiment_pipeline(x)[0]['label']
         y_pred.append(LABELS_MAPPING[PRETRAINED_MODEL][label])
 
     y_pred = le.transform(y_pred)
